328e2ab90d43a4fc6bdc7df092825cb007711bf5
devops-hunk <sanjaycsk77@gmail.com>
2026-07-12 20:21:08 +0530

    Update prime-number.py

--- app/prime-number.py
+++ app/prime-number.py
@@ -1,11 +1,9 @@
-## find first duplicate in the list:
+l = [10, 20, 30, 15, 20, 25, 26]
 
-l=[10,20,30,15,20,25,26]
-
-dict={}
+dict = {}
 
 for i in l:
     if i in dict:
         print("Duplicate found: {}".format(i))
     else:
-        dict[i]=1
+        dict[i] = 1
